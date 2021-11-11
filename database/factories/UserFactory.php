d8bbff627e5855efd3312f21d005277a3805cf2a
Jonathan Mejia <jonathna.mejia@herzum.com>
2021-11-10 21:13:54 -0500

    first commit

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = User::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => Hash::make('123456789'),
+            'remember_toke' => Str::random(10),
+            'api_token' => Str::random(50),
+        ];
+    }
+}
